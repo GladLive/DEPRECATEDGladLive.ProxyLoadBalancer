4c0c357c0eacfe3fcac7199be1c6e9443b87b7fd
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-02 03:33:15 -0500

    Documented PeerFactory delegate

--- src/GladLive.ProxyLoadBalancer/Session/Factory/PeerFactory.cs
+++ src/GladLive.ProxyLoadBalancer/Session/Factory/PeerFactory.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 
 namespace GladLive.ProxyLoadBalancer
 {
+	/// <summary>
+	/// Defined simplified delegate type for a factory delegate that produces <typeparamref name="TPeerType"/> peers.
+	/// </summary>
+	/// <typeparam name="TPeerType">The peer type to create.</typeparam>
+	/// <param name="sender">Network message sending service.</param>
+	/// <param name="details">Connection details that define the peer connection.</param>
+	/// <param name="subService">Message subscription service.</param>
+	/// <param name="disconnectHandler">Disconnection handler and subscription service.</param>
+	/// <returns>A new non-null instance of <typeparamref name="TPeerType"/> or throws.</returns>
 	public delegate TPeerType PeerFactory<TPeerType>(INetworkMessageSender sender, IConnectionDetails details, INetworkMessageSubscriptionService subService, IDisconnectionServiceHandler disconnectHandler)
 		where TPeerType : INetPeer;
 }
