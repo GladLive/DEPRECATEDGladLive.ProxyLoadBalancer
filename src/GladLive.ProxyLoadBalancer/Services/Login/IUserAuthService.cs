d1b62054fa9b432f8d0df1ce3d8375a677cc7e75
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-02 16:26:15 -0500

    Add IUserAuthService interface and AuthStat enum

---
+++ src/GladLive.ProxyLoadBalancer/Services/Login/IUserAuthService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladLive.ProxyLoadBalancer
+{
+	/// <summary>
+	/// Login/Authentication service contract.
+	/// </summary>
+	public interface IUserAuthService
+	{
+		/// <summary>
+		/// Attempts to handle an authentication request for a user login.
+		/// </summary>
+		/// <param name="ipOfUser">The <see cref="IPAddress"/> of the requesting user.</param>
+		/// <param name="userLoginString">User login string (Ex. Login name, email, accountname or etc)</param>
+		/// <param name="userPassword">Encrypted password of the user.</param>
+		/// <returns>This indicates only whether the Authservice is available. Not the success of the response.</returns>
+		AuthServiceState TryAuthenticateUser(IPAddress ipOfUser, string userLoginString, byte[] userPassword);
+	}
+}
