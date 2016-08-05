2356d317610ddfa2a64c5a1037ac9b08c4602a50
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-04 20:16:04 -0500

    Fixed AuthService GladNet2 2.x

--- src/GladLive.ProxyLoadBalancer/Session/Connection/AuthServiceClientPeer.cs
+++ src/GladLive.ProxyLoadBalancer/Session/Connection/AuthServiceClientPeer.cs
@@ -8,6 +8,11 @@ using Common.Logging;
 using Easyception;
 using GladLive.Common;
 using System.Net;
+using GladLive.Web.Payloads.Authentication;
+using GladNet.Engine.Common;
+using GladNet.Payload;
+using GladNet.Message;
+using GladNet.Message.Handlers;
 
 namespace GladLive.ProxyLoadBalancer
 {
@@ -19,7 +24,7 @@ namespace GladLive.ProxyLoadBalancer
 		/// <summary>
 		/// Response payload handling service for <see cref="PacketPayload"/> and <see cref="AuthServiceClientPeer"/> pairs.
 		/// </summary>
-		private IResponsePayloadHandlerService<AuthServiceClientPeer> responseHandlerService { get; }
+		private IResponseMessageHandlerService<AuthServiceClientPeer> responseHandlerService { get; }
 
 		/// <summary>
 		/// Creates a new object that represents an auth service connection.
@@ -30,13 +35,13 @@ namespace GladLive.ProxyLoadBalancer
 		/// <param name="netMessageSubService">Subscription service for incoming messages.</param>
 		/// <param name="disconnectHandler">Disconnection handler for the session.</param>
 		/// <param name="responseHandler">Request payload handler for the session.</param>
-		public AuthServiceClientPeer(ILog logger, INetworkMessageSender messageSender, IConnectionDetails details, 
-			INetworkMessageSubscriptionService subService, IDisconnectionServiceHandler disconnectHandler, IResponsePayloadHandlerService<AuthServiceClientPeer> responseHandler) 
-			: base(logger, messageSender, details, subService, disconnectHandler)
+		public AuthServiceClientPeer(ILog logger, INetworkMessageRouterService messageSender, IConnectionDetails details, 
+			INetworkMessageSubscriptionService subService, IDisconnectionServiceHandler disconnectHandler, IResponseMessageHandlerService<AuthServiceClientPeer> responseHandler, INetworkMessageRouteBackService routebackService) 
+			: base(logger, messageSender, details, subService, disconnectHandler, routebackService)
 		{
 			//We check logger null because we want to log now
-			Throw<ArgumentNullException>.If.IsNull(logger, nameof(logger), $"Logging service provided must be non-null.");
-			Throw<ArgumentNullException>.If.IsNull(responseHandler, nameof(responseHandler), $"Response handling service provided must be non-null.");
+			Throw<ArgumentNullException>.If.IsNull(logger)?.Now(nameof(logger), $"Logging service provided must be non-null.");
+			Throw<ArgumentNullException>.If.IsNull(responseHandler)?.Now(nameof(responseHandler), $"Response handling service provided must be non-null.");
 
 
 			//the authservice doesn't really 'connect'
@@ -49,9 +54,9 @@ namespace GladLive.ProxyLoadBalancer
 		/// <summary>
 		/// Called internally when an event is recieved. (setup using the subscription service)
 		/// </summary>
-		/// <param name="payload">Incoming payload.</param>
+		/// <param name="message">Incoming event message.</param>
 		/// <param name="parameters">Parameters the message was sent with (Null right now in this implementation).</param>
-		protected override void OnReceiveEvent(PacketPayload payload, IMessageParameters parameters)
+		protected override void OnReceiveEvent(IEventMessage message, IMessageParameters parameters)
 		{
 			//Right now web clients should recieve events
 			//There just isn't an implementation for it
@@ -61,31 +66,43 @@ namespace GladLive.ProxyLoadBalancer
 		/// <summary>
 		/// Called internally when a response is recieved. (setup using the subscription service)
 		/// </summary>
-		/// <param name="payload">Incoming payload.</param>
+		/// <param name="message">Incoming message.</param>
 		/// <param name="parameters">Parameters the message was sent with (Null right now in this implementation).</param>
-		protected override void OnReceiveResponse(PacketPayload payload, IMessageParameters parameters)
+		protected override void OnReceiveResponse(IResponseMessage message, IMessageParameters parameters)
 		{
 			//As of right now the implementation for the web client cannot provide message parameters.
 			//As such we should not check them, they're likely to be null until their implemented
 
 			//So, just check payloads
 			Throw<ArgumentNullException>.If
-				.IsNull(payload, nameof(payload), $"This indicates a critical error. The GladNet API has provided a null {nameof(PacketPayload)} to the {nameof(AuthServiceClientPeer.OnReceiveResponse)} method.");
+				.IsNull(message)?.Now(nameof(message), $"This indicates a critical error. The GladNet API has provided a null {nameof(IResponseMessage)} to the {nameof(AuthServiceClientPeer.OnReceiveResponse)} method.");
 
-			if(!responseHandlerService.TryProcessPayload(payload, parameters, this))
+			if(!responseHandlerService.TryProcessMessage(message, parameters, this))
 			{
 				//TODO: When message parameters are implemented for GladNet.ASP.Client we should include them in the logging statement.
-				Logger.Warn($"Failed to handle response message in {nameof(OnReceiveResponse)} for Type {nameof(AuthServiceClientPeer)} with PayloadType {payload.GetType().Name}.");
+				Logger.Warn($"Failed to handle response message in {nameof(OnReceiveResponse)} for Type {nameof(AuthServiceClientPeer)} with PayloadType {message.GetType().Name}.");
 			}
 		}
 
 		//implementation of: IUserAuthService
-		public AuthServiceState TryAuthenticateUser(IPAddress ipOfUser, string userLoginString, byte[] userPassword)
+		public void TryAuthenticateUser(IPAddress ipOfUser, string userLoginString, byte[] userPassword)
+		{
+			if (CheckServiceState() == AuthServiceState.Available)
+			{
+				//Build the GladNet payload for the AuthRequest to be send to the auth service.
+				AuthRequest request = new AuthRequest(ipOfUser, new LoginDetails(userLoginString, userPassword));
+
+				//Right now the web service is TCP, obviously, and only really can do reliable unordered without discard.
+				//This is a design fault of GladNet2 as it was never intended to be built on top of TCP/Web
+				//Also, if the SendResult is invalid we know something is wrong otherwise it's queued/sent.
+				SendRequest(request, DeliveryMethod.ReliableUnordered, true, 0);
+			}
+		}
+
+		public AuthServiceState CheckServiceState()
 		{
 			if (this.CanSend(OperationType.Request) && this.Status == NetStatus.Connected)
-			{
-				throw new NotImplementedException();
-			}
+				return AuthServiceState.Available;
 			else
 				return AuthServiceState.Unavailable;
 		}
